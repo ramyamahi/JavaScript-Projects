1608c010d91815838f02d91c85ef01826c7e945a
ramyamahi <ramyamahendran96@gmail.com>
2021-07-08 15:51:59 +0530

    Random choice picker

---
+++ Random-choice -picker/script.js
@@ -0,0 +1,19 @@
+const textarea = document.getElementById('textarea')
+const tagsElement = document.getElementById('tags')
+
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+})
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+    // console.log(tags)
+    tagsElement.innerHTML = ''
+
+    tags.forEach(tag => {
+        const tagElement = document.createElement('span')
+        tagElement.classList.add('tag')
+        tagElement.innerText = tag
+        tagsElement.appendChild(tagElement)
+    })
+}
